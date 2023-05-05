c5a8237a91231c17c934bfaef0053ade37b23b0b
nurgulkaya <nnurgulky@gmail.com>
2023-05-05 14:21:45 +0300

    add commit

---
+++ dikUcgeninHipotenusu.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class dikUcgeninHipotenusu {
+    public static void main(String[] args) {
+
+        int a, b;
+        double hipotenus, alan;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("İlk kenarın uzunluğunu giriniz: ");
+        a = input.nextInt();
+
+        System.out.print("İkinci kenarın uzunluğunu giriniz: ");
+        b = input.nextInt();
+
+        hipotenus = Math.sqrt((a * a) + (b * b));
+        alan = (a*b)/2;
+
+        System.out.println("Hipotenüs: " + hipotenus);
+        System.out.println("Üçgenin alanı = " + alan);
+    }
+}
+
