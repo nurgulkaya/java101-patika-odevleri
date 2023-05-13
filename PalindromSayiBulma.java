e8ac89a5a99d5de0bf05207f7b7eb380a4409a50
nurgulkaya <nnurgulky@gmail.com>
2023-05-13 15:46:21 +0300

    add commit

--- PalindromSayiBulma.java
+++ PalindromSayiBulma.java
@@ -1,4 +1,28 @@
-Patika Java Ödevleri
+import java.util.Scanner;
 
-1 - Artık Yıl Hesaplayan Program
-2 - 
+public class PalindromSayiBulma{
+        static boolean isPalindrom(int number){
+
+            int temp = number; 
+            int reverseNumber=0;
+            int lastNumber;
+
+            while (temp != 0){
+                lastNumber = temp %10;
+                reverseNumber = (reverseNumber * 10) + lastNumber;
+                temp /= 10;
+            }
+            if (number == reverseNumber)
+                return true;
+            else
+                return false;
+        }
+    
+        public static void main(String[] args) {
+            System.out.print("Bir sayı giriniz: ");
+            Scanner sc=new Scanner(System.in);
+            int number=sc.nextInt();
+            System.out.println(isPalindrom(number));
+        }
+    }
+
