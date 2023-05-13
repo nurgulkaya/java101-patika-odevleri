3f0f1be1f2f5213c87675a3514f2c02ac573c53c
nurgulkaya <nnurgulky@gmail.com>
2023-05-13 17:17:32 +0300

    add commit

---
+++ DesenegoreMetot.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DesenegoreMetot{
+    public static void patternMethod(int n){
+        System.out.print(n + " ");
+        if (n<=0){
+            return;
+        }
+        patternMethod(n-5);
+        System.out.print(n + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int n = input.nextInt();
+        patternMethod(n);
+        
+    }
+
+}
